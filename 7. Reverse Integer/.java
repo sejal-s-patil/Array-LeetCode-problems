d0c8be7fd22e00efbdeda1725a6912e8eb59b1e8
Sejal Patil <sejalpatil01112006@gmail.com>
2026-08-14 21:14:57 +0530

    Implement integer reversal with overflow checks

---
+++ 7. Reverse Integer/.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int reverse(int x) {
+
+        int lastDigit;
+        int result = 0 ;
+
+        while(x!=0){
+        lastDigit = x % 10;
+        x = x/10;
+
+    if (result > Integer.MAX_VALUE / 10 ||
+    (result == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
+    return 0;
+}
+    if (result < Integer.MIN_VALUE / 10 ||
+    (result == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
+    return 0;
+}
+
+        result = result*10 + lastDigit;
+        
+        }
+    return result;
+    }
+}
