dce97baec0219421108c0d48b16357cb0740d970
Sejal Patil <sejalpatil01112006@gmail.com>
2026-08-21 20:15:26 +0530

    Implement moveZeroes method to rearrange array

---
+++ 283. Move Zeroes/.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+    while(fast < nums.length){
+        if(nums[fast]!=0){
+            int temp = nums[fast];
+            nums[fast]=nums[slow];
+            nums[slow]=temp;
+
+            slow++;
+            fast++;
+        }
+        else{
+            fast++;
+        }
+    }
+        
+    }
+}
